662caf2c7940b1f5e951dd8eb28676e0a111a56c
bcggxgcx <135641968+bcggxgcx@users.noreply.github.com>
2023-12-02 19:28:00 +0330

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -6,15 +6,15 @@ class Development(Config):
     APP_ID = 13949344
     API_HASH = "408723003ad67fa8ab01ccc7f53e24ad"
     # the name to display in your alive message
-    ALIVE_NAME = "amirbechf"
+    ALIVE_NAME = "dgjyvnd"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://kkezimkw:g_llgfOwlK8OMrkmFDGC1hXLo3gz7ljP@peanut.db.elephantsql.com/kkezimkw"
+    DB_URI = "postgres://gvpzbwkt:0moN_-0ATs-rZEQ4TERir_CAEQBUOY0h@batyr.db.elephantsql.com/gvpzbwkt"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BJWap1sBu7ijv83rO53NSQbubowuYwYpKyOtfFah_v2fjvBj4c5q9maq5vU4jY6Btao0g021ju6VSpvOmvK7gGXSZ5R2zSvb_Y5DoB-tIflPZXjOZgypvVahe63hnDx_xjQGHLHlFVPFgDAojhCKdTz1UfbSnnXZmBzpoOwfAWNebyM6BTWvdLQYRFsOzbMtcBjdZzU1mgWNljbKOuJukYq17v7Ealv_RfP6spuvkgOO0mHvWzOcsrg4OMQ6EYGwg26p03HYPw1x-aSFQ05uhW6kwuPsdehPOMuQoziGh_qtXdQZ48WYbeJUT0sIF-bJIsHlMClEgW4iXdB98WfPut2OYUs68nk="
+    STRING_SESSION = "1BJWap1sBu3W0nB2Lb3ExfeZiSCA4OsYh0AxU2-U24uH7NYvIT_W7wAVuifoa2dObFnzjGCztzUxG2TXbjsgbX0Y8j0AGE79852GMioIy4AD75KObBVyLaoBvHD0bXY61tV7KWxwLtTes372eJcLIaZjg84zloiSvumw7jw3IE_sJs1sGYrpwlOonNPPS8LR9xdxb5ICbNQB8QCFjDuGTPiAz79NUHvoJgY1Y51ZtiONtt7-vRAItkO3SPioeiJ9f7-QU_cHiPbvmQ8CsT63hyEr5nbGm_9RON-Z4EjOJkNIv6CDzbf_cGktsMkRvIPeW0nntIwQjqYU0bjR-GyoEZ4zvtg60Gro="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "6443503095:AAHn0ZoH0NWbASVUqchZA5VA-KHf1G7wKi0"
+    TG_BOT_TOKEN = "6781223189:AAEczJdPDaUJr8Fooqf8hORyNpK3nmKaC8M"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001826716869
+    PRIVATE_GROUP_BOT_API_ID = -1001831825409
     # command handler
     COMMAND_HAND_LER = "."
     # command hanler for sudo
@@ -24,4 +24,4 @@ class Development(Config):
     # if you need badcat plugins set "True"
     BADCAT = "True"
     VCMODE = "True"
-    VC_SESSION = "1BJWap1sBu7ijv83rO53NSQbubowuYwYpKyOtfFah_v2fjvBj4c5q9maq5vU4jY6Btao0g021ju6VSpvOmvK7gGXSZ5R2zSvb_Y5DoB-tIflPZXjOZgypvVahe63hnDx_xjQGHLHlFVPFgDAojhCKdTz1UfbSnnXZmBzpoOwfAWNebyM6BTWvdLQYRFsOzbMtcBjdZzU1mgWNljbKOuJukYq17v7Ealv_RfP6spuvkgOO0mHvWzOcsrg4OMQ6EYGwg26p03HYPw1x-aSFQ05uhW6kwuPsdehPOMuQoziGh_qtXdQZ48WYbeJUT0sIF-bJIsHlMClEgW4iXdB98WfPut2OYUs68nk="
+    VC_SESSION = "1BJWap1sBu3W0nB2Lb3ExfeZiSCA4OsYh0AxU2-U24uH7NYvIT_W7wAVuifoa2dObFnzjGCztzUxG2TXbjsgbX0Y8j0AGE79852GMioIy4AD75KObBVyLaoBvHD0bXY61tV7KWxwLtTes372eJcLIaZjg84zloiSvumw7jw3IE_sJs1sGYrpwlOonNPPS8LR9xdxb5ICbNQB8QCFjDuGTPiAz79NUHvoJgY1Y51ZtiONtt7-vRAItkO3SPioeiJ9f7-QU_cHiPbvmQ8CsT63hyEr5nbGm_9RON-Z4EjOJkNIv6CDzbf_cGktsMkRvIPeW0nntIwQjqYU0bjR-GyoEZ4zvtg60Gro="
